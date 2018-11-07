4a6037aff1fc3814685770a900f0ee4cc7c56f27
Wameedh <wameedh.ali@hotmail.com>
2018-11-07 21:30:53 +0100

    add pro

--- client/src/api/modules.js
+++ client/src/api/modules.js
@@ -1,5 +1,23 @@
-const API_URL = 'http://localhost:4000'
+const API_URL = 'http://localhost:4000';
 
-export const getModules = () => {
-  return fetch(`${API_URL}/module`).then(response => response.json());
+const headers = {
+  'Content-Type': 'application/json'
+};
+
+export const getModules = () => fetch(`${API_URL}/module`).then(response => response.json());
+
+export const createModule = (title) => {
+  return fetch(`${API_URL}/module`, {
+    method: 'POST',
+    headers: headers, // this is where we set the headers
+    body: JSON.stringify({ title: title })
+  }).then(response => response.json());
+};
+
+export const DeleteModules = (title) => {
+  return fetch(`${API_URL}/module`, {
+    method: 'DELETE',
+    headers: headers, // this is where we set the headers
+    body: JSON.stringify({ title: title })
+  }).then(response => response.json());
 };
